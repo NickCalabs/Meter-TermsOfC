1714a42071e45a37cf2d467aebcc51a64957f6cc
AshwinGopalakrishnan <Ashwin@ashwins-mbp-3.lan>
2017-06-26 18:40:54 -0400

    init commit

---
+++ ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Meter TermsOfC
+//
+//  Created by Ashwin Gopalakrishnan on 6/24/17.
+//  Copyright © 2017 Ashwin Gopalakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        termsAndConTitle.text = "Hello Nick"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    @IBOutlet weak var termsAndConTitle: UITextView!
+    
+    
+    
+    @IBOutlet weak var TermsAndConBody: UITextView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
